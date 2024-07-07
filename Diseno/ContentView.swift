54cc3e5a7e01507744c24b79054f087a8e78dad9
AndresCR24 <c-andres-98@hotmail.com>
2024-07-07 17:24:03 -0500

    Initial Commit

---
+++ Diseno/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Diseno
+//
+//  Created by Andres David Cardenas Ramirez on 7/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Diseno/DisenoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisenoApp.swift
+//  Diseno
+//
+//  Created by Andres David Cardenas Ramirez on 7/07/24.
+//
+
+import SwiftUI
+
+@main
+struct DisenoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
